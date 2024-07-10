ebcc138b66103583e4e3c71a505406aa4fe19dae
Hager <hager1.essam@gmail.com>
2024-07-10 20:22:37 +0200

    Adding JS file

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-var list = document.getElementsByTagName("li");
 
-for (let i = 0; i < list.length; i++) {
-    let 
-}
+
+let addingBtn = document.getElementById("addingBtn");
+addingBtn.addEventListener("click", function () {
+    let Input = document.getElementById("input"),
+        tasksList = document.getElementById("tasksList"),
+        newtask = Input.value;
+
+    if (newtask.trim() !== "") {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(newtask));
+        tasksList.appendChild(li);
+        Input.value = "";
+
+        li.addEventListener("click", function () {
+            if (!li.classList.contains("done")) {
+                li.classList.add("done");
+                tasksList.appendChild(li);
+            }
+            else {
+                li.classList.remove("done");
+                let completedTasks = tasksList.querySelector("done");
+                if (completedTasks) {
+                    tasksList.insertBefore(li, completedTasks);
+                } else {
+                    tasksList.insertBefore(li, null);
+                }
+            }
+        });
+    }
+});
+
+
+
+
